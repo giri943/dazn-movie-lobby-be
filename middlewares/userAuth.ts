2950bace45f720f47eb39f6386424aa59f43a88f
Girish <girish.soman@schbang.com>
2024-03-10 20:32:48 +0530

    feat:added userAuth middleware

---
+++ middlewares/userAuth.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import User from '../models/Users';
+
+interface CustomRequest extends Request {
+  token?: string;
+  user?: any;
+}
+
+const userAuth = async (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    const token = req.header("Authorization")?.replace("Bearer ", "");    
+    if (!token) {
+      throw new Error();
+    }
+    const decoded: any = jwt.verify(token, process.env.JWT_SALT);  
+    console.log(decoded);
+
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });    
+    if (!user) {
+      throw new Error();
+    }
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send({ error: "Unauthorized request, Please Authenticate" });
+  }
+};
+
+export default userAuth;
